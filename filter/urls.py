cf27920d1ea2381cf2b02ec70941daec82f559ec
jwan16 <wanyichun@gmail.com>
2018-01-24 11:52:18 +0800

    Update urls.py

--- filter/urls.py
+++ filter/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 from .views import FilterDetail, CreateParam, UpdateParam, DeleteParam
-
+efkawnoewfnmwe
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)$', FilterDetail.as_view(), name='filterDetail'),
     url(r'^createparam/$', CreateParam),
